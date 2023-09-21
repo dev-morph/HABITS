e97ccbe145b76bfde39aa184ba77bff421c71a75
dev-morph <112484043+dev-morph@users.noreply.github.com>
2023-09-21 23:11:54 +0900

    dayjs backend 추가

--- backend/src/routines/routines.service.spec.ts
+++ backend/src/routines/routines.service.spec.ts
@@ -1,18 +1,37 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { RoutinesService } from './routines.service';
+import { Prisma } from '@prisma/client';
+import * as dayjs from 'dayjs';
 
 describe('RoutinesService', () => {
-  let service: RoutinesService;
+	let service: RoutinesService;
+	const routineWeeklyDummy: Prisma.RoutineCreateInput = {
+		user_email: 'weekly@test.com',
+		title: 'RUN THE TEST',
+		recur_pattern: 'W',
+		event_day: '0, 3, 5',
+		start_day: dayjs().format('YYYY-MM-DD'),
+		end_day: dayjs().format('YYYY-MM-DD'),
+	};
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [RoutinesService],
-    }).compile();
+	beforeEach(async () => {
+		const module: TestingModule = await Test.createTestingModule({
+			providers: [RoutinesService],
+		}).compile();
 
-    service = module.get<RoutinesService>(RoutinesService);
-  });
+		service = module.get<RoutinesService>(RoutinesService);
+	});
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
+	describe('루틴 Parsing 함수', () => {
+		it('Weekly 루틴 테스트', async () => {
+			const result = service.generateEventsByRoutine(routineWeeklyDummy);
+			expect(result).toBe(0);
+		});
+	});
+
+	// describe('루틴 조회', () => {
+	// 	it('루틴 리스트 조회 by user_email', async () => {
+
+	// 	})
+	// })
 });
--- backend/src/routines/routines.service.ts
+++ backend/src/routines/routines.service.ts
@@ -1,26 +1,33 @@
 import { Injectable } from '@nestjs/common';
 import { CreateRoutineDto } from './dto/create-routine.dto';
 import { UpdateRoutineDto } from './dto/update-routine.dto';
+import { Prisma } from '@prisma/client';
 
 @Injectable()
 export class RoutinesService {
-  create(createRoutineDto: CreateRoutineDto) {
-    return 'This action adds a new routine';
-  }
+	create(createRoutineDto: CreateRoutineDto) {
+		return 'This action adds a new routine';
+	}
 
-  findAll() {
-    return `This action returns all routines`;
-  }
+	findAll() {
+		return `This action returns all routines`;
+	}
 
-  findOne(id: number) {
-    return `This action returns a #${id} routine`;
-  }
+	findOne(id: number) {
+		return `This action returns a #${id} routine`;
+	}
 
-  update(id: number, updateRoutineDto: UpdateRoutineDto) {
-    return `This action updates a #${id} routine`;
-  }
+	update(id: number, updateRoutineDto: UpdateRoutineDto) {
+		return `This action updates a #${id} routine`;
+	}
 
-  remove(id: number) {
-    return `This action removes a #${id} routine`;
-  }
+	remove(id: number) {
+		return `This action removes a #${id} routine`;
+	}
+
+	generateEventsByRoutine(data: Prisma.RoutineCreateInput) {
+		const { start_day, event_day } = data;
+		//1. weekly, monthly, yearly 로 분리하여 처리.
+		return null;
+	}
 }
