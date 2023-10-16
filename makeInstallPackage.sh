99a87fc2c65d5572fd9d6c4c9888ecfa191cf81a
morphlee <morphlee@autocrypt.io>
2023-10-16 09:47:41 +0900

    installPackage.sh 추가 및 deploy.sh 삭제

---
+++ makeInstallPackage.sh
@@ -0,0 +1,15 @@
+# BASE
+IMAGE_PREFIX="habits_"
+PACKAGENAME="installPackage"
+
+mkdir ./$PACKAGENAME
+cp ./docker-compose.yml ./$PACKAGENAME
+
+IMAGE_LIST=$(docker images | grep $IMAGE_PREFIX | awk '{print $1":"$2}')
+
+for imgName in $IMAGE_LIST
+do
+	echo $imgName
+done
+
+docker save -o ./$PACKAGENAME/habits_images.tar $IMAGE_LIST
