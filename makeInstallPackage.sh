f9d1f1869095680e11de7cfa1d65c9e2961ce3be
morphlee <morphlee@autocrypt.io>
2023-10-20 12:50:01 +0900

    배포 로직 수정

--- makeInstallPackage.sh
+++ makeInstallPackage.sh
@@ -3,17 +3,17 @@ IMAGE_PREFIX="habits_"
 PACKAGENAME="installPackage"
 
 mkdir ./$PACKAGENAME
-cp ./docker-compose.yml ./$PACKAGENAME
-cp ./appspec.yml ./$PACKAGENAME
-cp ./deploy.sh ./$PACKAGENAME
+# cp ./docker-compose.yml ./$PACKAGENAME
+# cp ./appspec.yml ./$PACKAGENAME
+# cp ./deploy.sh ./$PACKAGENAME
 
-IMAGE_LIST=$(docker images | grep $IMAGE_PREFIX | awk '{print $1":"$2}')
+# IMAGE_LIST=$(docker images | grep $IMAGE_PREFIX | awk '{print $1":"$2}')
 
-for imgName in $IMAGE_LIST
-do
-	echo $imgName
-done
+# for imgName in $IMAGE_LIST
+# do
+# 	echo $imgName
+# done
 
-docker save -o ./$PACKAGENAME/habits_images.tar $IMAGE_LIST
-cd $PACKAGENAME
-tar -czvf ../$PACKAGENAME.tar.gz ./
+# docker save -o ./$PACKAGENAME/habits_images.tar $IMAGE_LIST
+# cd $PACKAGENAME
+tar -czvf ./$PACKAGENAME/$PACKAGENAME.tar.gz ./
