dfa8afc21fdfeffc0e8aa75c87f80f7d9fad1926
morphlee <morphlee@autocrypt.io>
2023-10-20 13:55:58 +0900

    스크립트 수정

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
+IMAGE_PREFIX="habits_"
 docker compose down
 IMAGE_LIST=$(docker images | grep $IMAGE_PREFIX | awk '{print $1":"$2}')
 # 기존 image 삭제
 docker rmi $IMAGE_LIST
-docker load --input /app/habits/habits_images.tar
 docker compose up -d
