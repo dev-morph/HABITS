6633453a1d5eeba21835266c74c488b74079a015
morphlee <morphlee@autocrypt.io>
2023-10-20 15:34:59 +0900

    secret 추가

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
-IMAGE_PREFIX="habits_"
-docker compose down
-IMAGE_LIST=$(docker images | grep $IMAGE_PREFIX | awk '{print $1":"$2}')
-# 기존 image 삭제
-docker rmi $IMAGE_LIST
-docker compose up -d
+echo "CURRENT PWD IS "$(pwd)
+# IMAGE_PREFIX="habits_"
+# docker compose down
+# IMAGE_LIST=$(docker images | grep $IMAGE_PREFIX | awk '{print $1":"$2}')
+# # 기존 image 삭제
+# docker rmi $IMAGE_LIST
+# docker compose up -d
