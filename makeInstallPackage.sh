87c34c3cf267e7cfec459e6c6609045558fb7357
dev-morph <112484043+dev-morph@users.noreply.github.com>
2023-10-21 12:38:43 +0900

    check

--- makeInstallPackage.sh
+++ makeInstallPackage.sh
@@ -1,9 +1,8 @@
 # BASE
+BASE_DIR_NAME="habits"
 IMAGE_PREFIX="habits_"
 PACKAGE_NAME="installPackage"
-PATH_NAME=$(pwd)
 
-echo $PATH_NAME
 cd ../
 mkdir $PACKAGE_NAME
-tar -czvf ./$PACKAGE_NAME/$PACKAGE_NAME.tar.gz $PATH_NAME
+tar -czvf $PATH_NAME/$PACKAGE_NAME.tar.gz $PATH_NAME
