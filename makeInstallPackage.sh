e4fb2c3ad9861c4b26ee173f2d2477bcbe5b50a9
dev-morph <112484043+dev-morph@users.noreply.github.com>
2023-10-21 12:46:44 +0900

    스크립트 수정

--- makeInstallPackage.sh
+++ makeInstallPackage.sh
@@ -1,8 +1,11 @@
 # BASE
-BASE_DIR_NAME="habits"
+BASE_DIR_NAME="HABITS"
 IMAGE_PREFIX="habits_"
 PACKAGE_NAME="installPackage"
 
 cd ../
 mkdir $PACKAGE_NAME
-tar -czvf $PATH_NAME/$PACKAGE_NAME.tar.gz $PATH_NAME
+tar -czvf ./$PACKAGE_NAME.tar.gz ./$BASE_DIR_NAME
+
+mv ./$PACKAGE_NAME.tar.gz ./$BASE_DIR_NAME
+rm -rf $PACKAGE_NAME
