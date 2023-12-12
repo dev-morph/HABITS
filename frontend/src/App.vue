b3985773eaa647910accf17c07dd11b5d6ae7a58
dev-morph <112484043+dev-morph@users.noreply.github.com>
2023-12-13 00:09:57 +0900

    frontend production 모드 스크립트 추가

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -21,6 +21,15 @@ import { useUserStore } from '@/store/user'
 const userStore = useUserStore()
 
 onBeforeMount(async () => {
+	const mode = import.meta.env.MODE;
+	console.log("mode is ", mode)
+
+	if(mode === "production"){
+		console.log("PRODUCTION MODE");
+		window.console.log = function(){};
+		window.console.warn = function(){};
+		window.console.error = function(){};
+	}
     await userStore.getUserInfo()
     userStore.setBg()
 })
